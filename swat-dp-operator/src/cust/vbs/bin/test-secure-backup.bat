66e11bb2e54dc30afda26587fdeb60e86e4197d8
Pierce Shah <psha@ch.ibm.com>
2016-06-19 17:51:14 +0200

    removed non-public projects

--- swat-dp-operator/src/cust/vbs/bin/test-secure-backup.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-
-set ANT_HOME=c:\vbsdev\tools\ant\apache-ant-1.8.4
-set DP_OP_HOME=c:\vbsdev\tools\DataPowerOperator
-
-set DP_ENV=test
-set SSL_OPTS=-Djavax.net.ssl.trustStore=%DP_OP_HOME%/certs/%DP_ENV%-cacerts -Djavax.net.ssl.trustStorePassword=changeit
-
-start %ANT_HOME%\bin\ant.bat %SSL_OPTS% -f %DP_OP_HOME%\build.xml bku_securebackup -Dsecurebackup.hostname=apalg11sa.vpnvbs2.admin.ch
